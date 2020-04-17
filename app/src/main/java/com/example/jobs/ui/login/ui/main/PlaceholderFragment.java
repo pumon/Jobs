eea34944eca5f22978746c08d1ddecc763e49d25
pumon <punith345@gmail.com>
2020-04-17 21:17:23 +0530

    UI fixes

--- app/src/main/java/com/example/jobs/ui/login/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/jobs/ui/login/ui/main/PlaceholderFragment.java
@@ -47,14 +47,8 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_job_main, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.create_job_fragment, container, false);
+
         return root;
     }
 }
